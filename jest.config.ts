1c6693983d29d0e64481a6a63d55f06b6f4a669a
Gustav Novotny <gustav.novotny@ableneo.com>
2026-03-18 00:24:43 +0100

    chore: fix package.json dep placement and remove unused ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,9 +6,6 @@ const createJestConfig = nextJest({ dir: './' })
 const config: Config = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
 }
 
 export default createJestConfig(config)
